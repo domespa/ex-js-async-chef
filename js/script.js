e8cd70a2a63393d1707b70c612dcb5f03392564b
domespa <dumiii1988@gmail.com>
2025-04-09 09:55:22 +0200

    done

---
+++ js/script.js
@@ -0,0 +1,37 @@
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
+}
+
+async function getChefBirthday(id) {
+  let ricetta;
+
+  try {
+    ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+    console.log(ricetta);
+  } catch (error) {
+    throw new Error(`Problemi nel recuperare la ricetta con id ${id}`);
+  }
+
+  if (ricetta.message) {
+    throw new Error(ricetta.message);
+  }
+
+  try {
+    let user = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`);
+    console.log(user);
+    return user.birthDate;
+  } catch (error) {
+    throw new Error(`Problemi nel recuperare lo chef con id ${ricetta.userId}`);
+  }
+}
+
+getChefBirthday(5)
+  .then((birthDay) =>
+    console.log(
+      "Data di nascita dello chef:",
+      dayjs(birthDay).format("DD/MM/YYYY")
+    )
+  )
+  .catch((error) => console.error(error));
